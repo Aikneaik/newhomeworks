bd83b1e7c0f9119811bc2651ec1a2e9b013150ae
aikneaik <aikarakelyan00@gmail.com>
2026-09-02 01:39:19 +0300

    Правки

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 const (
-	usdEur = 0.85
-	usdRub = 85
+	usdEur         = 0.85
+	usdRub         = 85
+	eurRub float64 = usdRub / usdEur
 )
 
 func main() {
-	eurRub2 := usdRub / usdEur
 	fmt.Println(eurRub)
 }
